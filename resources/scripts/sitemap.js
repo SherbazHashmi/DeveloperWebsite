78d15f78ce12ec83a41ac2ac807024dd29bc3849
Sherbaz <sherbaz.hashmi@gmail.com>
2019-03-20 13:45:24 +1100

    Added Sitemap Styling and basic logic

---
+++ resources/scripts/sitemap.js
@@ -0,0 +1,9 @@
+const currentSelection = 'tab-home';
+const descriptions = {
+    'tab-home': 'Home Description Here',
+    'tab-blog': 'Blog Description Here',
+    'tab-map': 'Map Description here'
+}
+function changeSelection(element) {
+    console.log(element.id);
+}
